662f66aedd53fcd514bc4532b15501d0a232af00
Akhil krishna <akhilkrishna1301@gamil.com>
2023-01-02 09:30:54 +0530

    generic view by class

--- todo_app/views.py
+++ todo_app/views.py
@@ -2,7 +2,36 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . models import Task
 from .forms import Todoforms
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView,DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
+
+#class based
+class TaskListView(ListView):
+    model=Task
+    template_name='home.html'
+    context_object_name='obj1'
+
+class TaskDetailView(DetailView):
+    model=Task
+    template_name='detail.html'
+    context_object_name='i'  
+
+class TaskUpdateView(UpdateView):
+    model=Task
+    template_name='update.html'
+    fields=('name','priority','date') 
+    def get_success_url(self):
+        return reverse_lazy('cbvdetail',kwargs={'pk':self.object.id})
+class TaskDeleteView(DeleteView):
+    model=Task
+    template_name='delete.html'
+    success_url= reverse_lazy('cbvtask')  
+    
+    #function based 
+
 def result(request):
     obj1=Task.objects.all()
     if request.method=='POST':
